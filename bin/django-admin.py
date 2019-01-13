a6e4dea511a5f5bc08637e0faabc81e448f34c72
a-bertrand <antho.bertrand101@gmail.com>
2019-01-13 16:21:37 +0100

    addbin

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/anthonybertrand/Documents/Dev/musicschool/musicschool/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
