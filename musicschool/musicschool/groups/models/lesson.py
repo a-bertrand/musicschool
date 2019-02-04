cf08d06f255128183e757be3735db0549b81f61a
a-bertrand <a.bertrand@zero-gachis.com>
2019-02-04 16:15:23 +0100

    add last returrrrn

---
+++ musicschool/musicschool/groups/models/lesson.py
@@ -0,0 +1,22 @@
+"""
+    cours :
+        choisir un eleve 
+        lui dire tous les lundi 
+        ajouter tous les lundi de l'année 
+        
+        
+    eleve 
+        
+    class Cours
+        Label
+        Date 
+        User 
+
+    
+    def create(jour):
+        for each lundi dans l'année 
+            self.addTimeStamp(date)
+
+    class TimestampModel()
+        date_meet = DateTimeField()
+"""
--- musicschool/musicschool/groups/models/media.py
+++ musicschool/musicschool/groups/models/media.py
@@ -2,12 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
 
+
 # Create your models here.
+# TODO READ TO ADD Guitar pro Lecteur 
+# https://ourcodeworld.com/articles/read/399/how-to-display-guitar-pro-tabs-in-the-browser-with-javascript-using-alphatab
 class Media(models.Model):
-    name    = models.CharField(max_length=30)
-    video   = models.FileField(upload_to='videos', null=True, blank=True)
-    image   = models.FileField(upload_to='images', null=True, blank=True )
-    text 	= models.TextField(max_length=2000, null=True, blank=True)
+    name        = models.CharField(max_length=30)
+    video       = models.FileField(upload_to='videos',  null=True, blank=True)
+    image       = models.FileField(upload_to='images',  null=True, blank=True)
+    pdf         = models.FileField(upload_to='pdf',     null=True, blank=True)
+    audio       = models.FileField(upload_to='pdf',     null=True, blank=True)
+    guitarpro   = models.FileField(upload_to='pdf',     null=True, blank=True)
+    youtubeurl  = models.CharField(max_length=50,       null=True, blank=True)
+    text 	    = models.TextField(max_length=2000,     null=True, blank=True)
 
     def __str__(self):
         return self.name
